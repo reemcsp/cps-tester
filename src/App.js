8e7054e68b148cfc2884aa6bf4a386a7f9a0dd08
reemcsp <reem092006@gmail.com>
2020-08-14 12:05:55 +0300

    Timer now starts on first click

--- src/App.js
+++ src/App.js
@@ -1,54 +1,31 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import { Typography, Button } from 'antd';
+import CountDown from 'ant-design-pro/lib/CountDown';
 
-
-function useInterval(callback, delay) {
-  const savedCallback = useRef();
-
-  // Remember the latest callback.
-  useEffect(() => {
-    savedCallback.current = callback;
-  }, [callback]);
-
-  // Set up the interval.
-  useEffect(() => {
-    function tick() {
-      savedCallback.current();
-    }
-    if (delay !== null) {
-      let id = setInterval(tick, delay);
-      return () => clearInterval(id);
-    }
-  }, [delay]);
-}
 
 function App() {
+  const [initialDate, setInitialDate] = useState();
+  const [firstActivated, setFirstActivated] = useState(false);
   const [isActive, setIsActive] = useState(true);
-  const [seconds, setSeconds] = useState(5);
-  function decreaseSecond() {
-    setSeconds(seconds - 1)
+  const [clicks, setClicks] = useState(0);
+
+  function increaseClicks() {
+    if(clicks === 0) {
+      setFirstActivated(true)
+      setInitialDate(new Date().getTime());
+    }
+    setClicks(clicks + 1)
   }
 
-  useInterval(() => {
-    if (seconds > 0) {
-      decreaseSecond()
-    }
-    if (seconds === 0) {
-      setIsActive(false)
-    }
-
-  }, 1000);
-
-  const [clicks, setClicks] = useState(0);
-  function increaseClicks() {
-    setClicks(clicks + 1)
+  function onTimerEnd (){
+    setIsActive(false)
   }
 
   return (
     <div id="page">
       <Typography.Title id="title">⏱ cps tester</Typography.Title>
-      <Typography.Title id="timer">{seconds}</Typography.Title>
+      {firstActivated && <CountDown  target={initialDate + 6000} onEnd={onTimerEnd} />}
       <Button onClick={increaseClicks} disabled={!isActive} type="primary" size="large">Click Me</Button>
       <Typography.Text strong id="result">Result: {clicks / 5} cps!</Typography.Text>
     </div >
